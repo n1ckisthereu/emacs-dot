4c4915da28e32b3718bedebb0e009e105ac6b9ce
nickx00 <75399141+n1ckisthereu@users.noreply.github.com>
2025-08-07 16:07:03 -0300

    feat(ui): change colerscheme to doom emacs (atom)

--- config/ui-cfg.el
+++ config/ui-cfg.el
@@ -1,12 +1,22 @@
 ;; Colorscheme
-(use-package catppuccin-theme
-  :config
-  (load-theme 'catppuccin t)
-  (catppuccin-set-color 'base "#0e1119")
-  (catppuccin-reload)
+(use-package doom-themes
+  :ensure t
   :custom
-  (catppuccin-flavor 'mocha)
-  :ensure t)
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;; for treemacs users
+  (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  :config
+  (load-theme 'doom-one t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  ;; (doom-themes-neotree-config)
+  ;; or for treemacs users
+  ;; (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 ; Doom Modeline
 (use-package doom-modeline
