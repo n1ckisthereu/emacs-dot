4e7fea68606f19c81dc8145db82cb806e0637427
nickx00 <75399141+n1ckisthereu@users.noreply.github.com>
2025-08-08 23:43:05 -0300

    refactor(init): use pkg emacs on init and configure mb to vertico

--- init.el
+++ init.el
@@ -1,8 +1,23 @@
-; Load my configuration directory
+;;; Load my configuration from the directory config
 (add-to-list 'load-path (locate-user-emacs-file "config/"))
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(setq backup-directory-alist '((user-emacs-directory . "backups")))
+; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+  ;; to switch display modes.
+  (context-menu-mode t)
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt))
+
+  (custom-file (locate-user-emacs-file "custom.el")))
 
 (require 'melpa-cfg)
 (require 'ui-cfg)
